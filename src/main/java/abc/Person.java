d3fc4e51ccc637be0799f2dcee462881b71cf257
gitxiaobai <krisandpeters@qq.com>
2018-06-14 11:30:59 +0800

    "Second commit"

---
+++ src/main/java/abc/Person.java
@@ -0,0 +1,15 @@
+package abc;
+
+public class Person {
+
+	public static void main(String[] args) {
+		
+		show("Author");
+		
+	}
+	
+	public static void show(String str) {
+		System.out.println("Hello GitHub!" + str);
+	}
+
+}
